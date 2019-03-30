19a6e86b53c5f9a21e14129ea93afd1e14dbd8f1
unknown <suvajit.c@outlook.com>
2019-03-30 20:01:02 +0530

    fixed serverinfo command

--- src/commands/serverinfo.js
+++ src/commands/serverinfo.js
@@ -12,29 +12,29 @@ class ServerCommand extends Command {
     }
 
     exec(message) {
-         var randomColor = "#000000".replace(/0/g,function(){return (~~(Math.random()*16)).toString(16);});
+        const randomColor = "#000000".replace(/0/g,function(){return (~~(Math.random()*16)).toString(16);});
 
-    const exampleEmbed = new Discord.RichEmbed()
-	.setColor(randomColor)
-	.setTitle('Server Info')
-     .setThumbnail(message.guild.iconURL)
-	.setAuthor(`${message.author.username}#${message.author.discriminator}`)
-	.setDescription('Info On Your Server')
-     .addField('❯ Name', message.guild.name, true)
-    .addField("❯ Guild ID",  message.guild.id, true)
-	.addField("❯ Guild Creation",  moment.utc(message.guild.createdAt).format('MM/DD/YYYY h:mm A'), true)
-    .addField('❯ Members',  message.guild.memberCount, true)
-    .addField('❯ Explicit Filter', filterLevels[message.guild.explicitContentFilter], true)
-.addField('❯ Verification Level', verificationLevels[message.guild.verificationLevel], true)
-    .addField('❯ Channels',  message.guild.channels.filter(channel => channel.type !== 'category').size, true)
-    .addField('❯ Server Owner',  message.guild.owner, true)
-    .addField('❯ Server Region',  message.guild.region.toUpperCase(), true)
-    .addField('❯ Join Date', moment.utc(message.member.joinedAt).format('MM/DD/YYYY h:mm A'), true)
-	.setTimestamp()
-    .setImage(message.guild.iconURL)
-	.setFooter(new Date().getTime() - message.createdTimestamp + " ms");
+        const exampleEmbed = new Discord.MessageEmbed()
+	    .setColor(randomColor)
+	    .setTitle('Server Info')
+        .setThumbnail(message.guild.iconURL())
+	    .setAuthor(`${message.author.tag}`)
+	    .setDescription('Info On Your Server')
+        .addField('❯ Name', message.guild.name, true)
+        .addField("❯ Guild ID",  message.guild.id, true)
+	    .addField("❯ Guild Creation",  moment.utc(message.guild.createdAt).format('MM/DD/YYYY h:mm A'), true)
+        .addField('❯ Members',  message.guild.memberCount, true)
+        .addField('❯ Explicit Filter', filterLevels[message.guild.explicitContentFilter], true)
+        .addField('❯ Verification Level', verificationLevels[message.guild.verificationLevel], true)
+        .addField('❯ Channels',  message.guild.channels.filter(channel => channel.type !== 'category').size, true)
+        .addField('❯ Server Owner',  message.guild.owner, true)
+        .addField('❯ Server Region',  message.guild.region.toUpperCase(), true)
+        .addField('❯ Join Date', moment.utc(message.member.joinedAt).format('MM/DD/YYYY h:mm A'), true)
+	    .setTimestamp()
+        .setImage(message.guild.iconURL())
+	    .setFooter(new Date().getTime() - message.createdTimestamp + " ms");
 
-message.channel.send(exampleEmbed);
+        return message.channel.send(exampleEmbed);
     }
 }
 
