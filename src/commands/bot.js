e842fe3760c3b2d8db16c1709f983c923cb7b1a3
unknown <suvajit.c@outlook.com>
2019-03-30 21:01:21 +0530

    js beautify

--- src/commands/bot.js
+++ src/commands/bot.js
@@ -10,27 +10,26 @@ class BotInfoCommand extends Command {
     }
 
     exec(message) {
-         var randomColor = "#000000".replace(/0/g,function(){return (~~(Math.random()*16)).toString(16);});
-    const exampleEmbed = new Discord.MessageEmbed()
-	.setColor(randomColor)
-	.setTitle('Bot Info')
-	.setAuthor(`${message.author.username}#${message.author.discriminator}`)
-	.setDescription('Info On Your Server')
-    .addField("Bot Name",  'Moni.js', true)
-	.addField("Created On",  this.client.user.createdAt, true)
-    .addField('Serving',  `${this.client.users.size} Users`, true)
-    .addField('Channel Count',  this.client.channels.size, true)
-    .addField('Bot Dev',  'Moni#3701', true)
-    .addField('Guild Size',  this.client.guilds.size, true)
-    .addField('UpTime', `${this.client.uptime} ms`, true)
-    .addField('Last Restart', this.client.readyAt, true)
-    .addField('Discord.js', Discord.version, true)
-    .addField('Akairo', version, true)
-	.setTimestamp()
-    .setImage(this.client.user.avatarURL)
-	.setFooter("Version 0.0.2");
-
-message.channel.send(exampleEmbed);
+        let randomColor = "#000000".replace(/0/g,function(){return (~~(Math.random()*16)).toString(16);});
+        const exampleEmbed = new Discord.MessageEmbed()
+	    .setColor(randomColor)
+	    .setTitle('Bot Info')
+	    .setAuthor(`${message.author.username}#${message.author.discriminator}`)
+	    .setDescription('Info On Your Server')
+        .addField("Bot Name",  'Moni.js', true)
+	    .addField("Created On",  this.client.user.createdAt, true)
+        .addField('Serving',  `${this.client.users.size} Users`, true)
+        .addField('Channel Count',  this.client.channels.size, true)
+        .addField('Bot Dev',  'Moni#3701', true)
+        .addField('Guild Size',  this.client.guilds.size, true)
+        .addField('UpTime', `${this.client.uptime} ms`, true)
+        .addField('Last Restart', this.client.readyAt, true)
+        .addField('Discord.js', Discord.version, true)
+        .addField('Akairo', version, true)
+	    .setTimestamp()
+        .setImage(this.client.user.avatarURL)
+	    .setFooter("Version 0.0.2");
+        return message.util.send(exampleEmbed);
     }
 }
 
