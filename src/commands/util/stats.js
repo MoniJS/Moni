2e6bd142d082aa9537db7577a0a72f6758254b3f
Suvajit <suvajit.c@outlook.com>
2019-04-01 13:12:04 +0530

    file renamed

--- src/commands/util/stats.js
+++ src/commands/util/stats.js
@@ -2,13 +2,13 @@ const moment = require('moment'); require('moment-duration-format');
 const { Command } = require('discord-akairo');
 const Discord = require("discord.js");
 const Akairo = require('discord-akairo');
-const { version } = require('../../package.json');
+const { version } = require('../../../package.json');
 const os = require('os-utils')
 
 class BotInfoCommand extends Command {
     constructor() {
         super('BotInfo', {
-            aliases: ['bot', 'botstats', 'botinfo'],
+            aliases: ['stats', 'botstats', 'botinfo', 'bot'],
             category: 'util',
             channel: 'guild',
             clientPermissions: ['SEND_MESSAGES', 'EMBED_LINKS'],
@@ -29,7 +29,7 @@ class BotInfoCommand extends Command {
 	    .addField("Created On",  moment.utc(this.client.user.createdAt).format('DD-MM-YYYY kk:mm:ss'), true)
         .addField('Serving',  `${this.client.users.size} Users`, true)
         .addField('Channel Count',  this.client.channels.size, true)
-        .addField('Bot Devs',  devs.join('| '), true)
+        .addField('Bot Devs',  devs.join(' | '), true)
         .addField('Guild Size',  this.client.guilds.size, true)
         .addField('UpTime', moment.duration(this.client.uptime).format("M [months], W [weeks], D [days], H [hrs], m [mins], s [secs]"), true)
         .addField('Last Restart', moment.utc(this.client.readyAt).format('DD-MM-YYYY kk:mm:ss'), true)
