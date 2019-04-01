ef2027ad544f3360733ee37755fe7af1a3353b1b
Suvajit <suvajit.c@outlook.com>
2019-04-01 13:39:14 +0530

    modified stats command

--- src/commands/util/stats.js
+++ src/commands/util/stats.js
@@ -22,26 +22,25 @@ class BotInfoCommand extends Command {
         const devs = ['447916021198749696', '444432489818357760'].map(id => this.client.users.get(id).tag);
         const embed = new Discord.MessageEmbed()
 	    .setColor('RANDOM')
-	    .setTitle('Bot Info')
-	    .setAuthor(`${message.author.tag}`)
-	    .setDescription('Info On Your Server')
-        .addField("Bot Name",  'Moni.js', true)
-	    .addField("Created On",  moment.utc(this.client.user.createdAt).format('DD-MM-YYYY kk:mm:ss'), true)
-        .addField('Serving',  `${this.client.users.size} Users`, true)
-        .addField('Channel Count',  this.client.channels.size, true)
-        .addField('Bot Devs',  devs.join(' | '), true)
-        .addField('Guild Size',  this.client.guilds.size, true)
-        .addField('UpTime', moment.duration(this.client.uptime).format("M [months], W [weeks], D [days], H [hrs], m [mins], s [secs]"), true)
-        .addField('Last Restart', moment.utc(this.client.readyAt).format('DD-MM-YYYY kk:mm:ss'), true)
-        .addField('Discord.js', Discord.version, true)
-        .addField('Akairo', Akairo.version, true)
+        .setAuthor(`${this.client.user.username} Statistics`)
+        .addField('Bot Devs',  devs.join(' | '))
         .addField('Memory',
-[`• Using : ${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)} MB`,
-`• Free : ${Math.round(os.freemem())}`
-], true)
+            [`• Using : ${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)} MB`,
+            `• Free : ${Math.round(os.freemem())}`
+        ])
+        .addField('UpTime', moment.duration(this.client.uptime).format("M [months], W [weeks], D [days], H [hrs], m [mins], s [secs]"), true)
+        .addField('General Stats', [
+            `Guilds: ${this.client.guilds.size}`,
+            `Channels: ${this.client.channels.size}`,
+            `Users: ${this.client.users.size}`,
+        ])
+        .addField('Library', [
+            `[Discord.js (v${Discord.version})](https://discord.js.org)`,
+            `[Akairo (v${Akairo.version})](https://discord-akairo.github.io)`
+        ], true)
 	    .setTimestamp()
-        .setImage(this.client.user.avatarURL)
-	    .setFooter(`Version ${version}`);
+        .setThumbnail(this.client.user.avatarURL)
+	    .setFooter(`Version ${version}`, this.client.user.avatarURL);
         return message.util.send(embed);
     }
 }
