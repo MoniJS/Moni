f6a924e102cf4f718d96b4f760570e2446bceec2
Moni.S <40552237+MoniS10@users.noreply.github.com>
2019-04-13 17:59:48 -0400

    Restart the bot!!!

---
+++ src/commands/owner/restart.js
@@ -0,0 +1,28 @@
+const { Command } = require('discord-akairo');
+const { Util, MessageEmbed } = require('discord.js');
+
+
+
+
+class RestartCommand extends Command {
+	constructor() {
+		super('restart', {
+			aliases: ['restart'],
+			category: 'owner',
+			ownerOnly: true,
+			quoted: false,
+			description: {
+				content: 'Restarts Bot',
+				usage: '!restart'
+			}
+		});
+	}
+
+     async exec(message) {
+          process.exit();
+     }
+
+
+}
+
+module.exports = RestartCommand;
