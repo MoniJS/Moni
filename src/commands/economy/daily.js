edb728e6425017620fd876f2eabff8bd959becbb
Moni.S <40552237+MoniS10@users.noreply.github.com>
2019-04-13 18:21:53 -0400

    Client Balance cmd

---
+++ src/commands/economy/daily.js
@@ -0,0 +1,33 @@
+const discord = require('discord.js');
+const eco = require('discord-economy');
+const { Command } = require('discord-akairo');
+
+class DailyCommand extends Command {
+    constructor() {
+        super('Daily', {
+            aliases: ['Daily'],
+            category: 'economy',
+            channel: 'guild',
+            clientPermissions: ['SEND_MESSAGES'],
+            description: {
+                content: 'Get your daily bonus',
+                examples: ['@Moni#2030 daily']
+            }
+        });
+    }
+
+    exec(message) {
+         var output = await eco.Daily(message.author.id)
+
+      if (output.updated) {
+
+        var profile = await eco.AddToBalance(message.author.id, 100)
+        message.reply(`Nice job checking in today for you points. You now have ${profile.newbalance} points.`);
+
+      } else {
+        message.channel.send(`Hey! You. :rage: Trying to cheat the system. Nope.\nYour next daily will be in ${output.timetowait}`)
+      }
+    }
+}
+
+module.exports = DailyCommand;
