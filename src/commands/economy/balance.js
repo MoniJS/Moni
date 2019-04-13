9be62aa7d5d6ad3263ea1db635396acb8fb42397
Moni.S <40552237+MoniS10@users.noreply.github.com>
2019-04-13 18:15:38 -0400

    Add Balance Command

---
+++ src/commands/economy/balance.js
@@ -0,0 +1,25 @@
+const discord = require('discord.js');
+const eco = require('discord-economy');
+const { Command } = require('discord-akairo');
+
+class BalanceCommand extends Command {
+    constructor() {
+        super('Balance', {
+            aliases: ['balance'],
+            category: 'economy',
+            channel: 'guild',
+            clientPermissions: ['SEND_MESSAGES'],
+            description: {
+                content: 'See how much money you have saved up :)',
+                examples: ['@Moni#2030 balance']
+            }
+        });
+    }
+
+    exec(message) {
+         var output = await eco.FetchBalance(message.author.id)
+        message.channel.send(`Yo ${message.author.tag}! My senses say you own ${output.balance} points.`);
+    }
+}
+
+module.exports = BalanceCommand;
