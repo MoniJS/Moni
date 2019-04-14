efa049308084ef85463abb716ebeaa62cac22006
Moni.S <40552237+MoniS10@users.noreply.github.com>
2019-04-14 17:32:35 -0400

    prettier

--- src/commands/dev/github.js
+++ src/commands/dev/github.js
@@ -1,26 +1,26 @@
-const { Command } = require('discord-akairo');
+const { Command } = require("discord-akairo");
 
 class GitHubCommand extends Command {
-    constructor() {
-        super('Github', {
-            aliases: ['github', 'git'],
-            category: 'dev',
-            channel: 'guild',
-            clientPermissions: ['SEND_MESSAGES'],
-            description: {
-                content: 'GitHub Links',
-                examples: ['']
-            }
-        });
-    }
+  constructor() {
+    super("Github", {
+      aliases: ["github", "git"],
+      category: "dev",
+      channel: "guild",
+      clientPermissions: ["SEND_MESSAGES"],
+      description: {
+        content: "GitHub Links",
+        examples: [""]
+      }
+    });
+  }
 
-    exec(message) {
-        if(message.guild.id === "550140222822809610") {
-            return message.util.send([
-                `Here is the JavaScript GitHub Url => (<https://github.com/moni-js>)`,
-            ]);
-        }
+  exec(message) {
+    if (message.guild.id === "550140222822809610") {
+      return message.util.send([
+        `Here is the JavaScript GitHub Url => (<https://github.com/moni-js>)`
+      ]);
     }
+  }
 }
 
 module.exports = GitHubCommand;
--- src/commands/dev/javascript.js
+++ src/commands/dev/javascript.js
@@ -1,32 +1,32 @@
-const { Command } = require('discord-akairo');
+const { Command } = require("discord-akairo");
 
 class JavaScriptCommand extends Command {
-    constructor() {
-        super('JavaScript', {
-            aliases: ['Javascript', 'js','ecmascript'],
-            category: 'dev',
-            channel: 'guild',
-            clientPermissions: ['SEND_MESSAGES'],
-            description: {
-                content: 'Learn JS',
-                examples: ['']
-            }
-        });
-    }
+  constructor() {
+    super("JavaScript", {
+      aliases: ["Javascript", "js", "ecmascript"],
+      category: "dev",
+      channel: "guild",
+      clientPermissions: ["SEND_MESSAGES"],
+      description: {
+        content: "Learn JS",
+        examples: [""]
+      }
+    });
+  }
 
-    exec(message) {
-        if(message.guild.id === '550140222822809610') {
-            return message.util.send([
-                `**Learn JS**`,
-                `- MDN JS : <https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide>`,
-                `- CodeAcademy : <https://www.codecademy.com/learn/introduction-to-javascript>`,
-                `- Eloquent JavaScript : <http://eloquentjavascript.net/>`,
-                `- JavaScript For Cats : <http://jsforcats.com/>`,
-                `- YDKJ : <https://github.com/getify/You-Dont-Know-JS>`,
-                `- JS-Books : <https://jsbooks.revolunet.com/>`
-            ]);
-        }
+  exec(message) {
+    if (message.guild.id === "550140222822809610") {
+      return message.util.send([
+        `**Learn JS**`,
+        `- MDN JS : <https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide>`,
+        `- CodeAcademy : <https://www.codecademy.com/learn/introduction-to-javascript>`,
+        `- Eloquent JavaScript : <http://eloquentjavascript.net/>`,
+        `- JavaScript For Cats : <http://jsforcats.com/>`,
+        `- YDKJ : <https://github.com/getify/You-Dont-Know-JS>`,
+        `- JS-Books : <https://jsbooks.revolunet.com/>`
+      ]);
     }
+  }
 }
 
 module.exports = JavaScriptCommand;
