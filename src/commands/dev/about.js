3faf9c18b97c5a890604507db4fff7213fa514f6
Moni.S <40552237+MoniS10@users.noreply.github.com>
2019-04-17 08:52:12 -0400

    About.js added for the bot. About the bot.

---
+++ src/commands/dev/about.js
@@ -0,0 +1,34 @@
+const { Command } = require('discord-akairo');
+const Akairo = require('discord-akairo');
+const Discord = require('discord.js');
+const fs = require('fs');
+
+class AboutCommand extends Command {
+	constructor() {
+		super('about', {
+			aliases: ['about', 'info'],
+			category: 'general',
+			clientPermissions: ['EMBED_LINKS'],
+			description: { content: 'Shows information about the bot.' }
+		});
+	}
+
+	exec(message) {
+		const prefix = this.handler.prefix(message);
+		const owner = this.client.users.get(this.client.ownerID);
+
+		const embed = this.client.util.embed()
+			.setColor('RANDOM')
+			.setTitle(`About ${this.client.user.username}`)
+			.addField('Dev', `**${this.client.user.username}** is developed by **[Moni#3701](https://github.com/MoniS10)**`)
+			.addField('Core', `[Discord.js](https://discord.js.org) (${Discord.version}) library and [Akairo](https://1computer1.github.io/discord-akairo) (${Akairo.version.replace(/beta.1/g, 'dev')}) framework`)
+			//.addField('Music', `[Lavalink](https://github.com/lavalibs/lavalink.js) audio player, along with [Lavaqueue](https://github.com/lavalibs/lavaqueue), backed by [Redis](https://github.com/MicrosoftArchive/redis) instance`)
+			.addField('Database', `[Sequelize](http://docs.sequelizejs.com/) [Node.js](https://nodejs.org/en/) ORM`)
+			.addField('Host', [
+				`[Amazon EC2](https://aws.amazon.com/ec2/)`,
+			])
+		return message.util.send({ embed });
+	}
+}
+
+module.exports = AboutCommand;
