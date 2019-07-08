62e87271c0692661f91808d53200c62a6a58a2d5
Moni <40552237+MoniS10@users.noreply.github.com>
2019-07-08 09:30:03 -0400

    Update pull.js

--- src/commands/general/pull.js
+++ src/commands/general/pull.js
@@ -5,7 +5,7 @@ class PullCommand extends Command {
 		super('pull', {
 			aliases: ['pull', 'fetch],
 			category: 'general',
-			clientPermissions: ['READ_MESSAGES'],
+			clientPermissions: ['READ_MESSAGES', 'SEND_MESSAGES'],
 			description: {
 				content: 'Fetch latest src from Github'
 			}
@@ -14,7 +14,12 @@ class PullCommand extends Command {
 
 	exec(message) {
 		  const shell = require ('shelljs');
-      shell.exec('git pull git@github.com:monijs/moni.git');
+      		 const {stderr, stdout, code }shell.exec('git pull git@github.com:monijs/moni.git');
+		return message.channel.send([
+			`${stderr}`,
+			`${stdout}`,
+			`Process exited with code ${code}`
+		], { code: true, split: true });
 	}
 }
 
